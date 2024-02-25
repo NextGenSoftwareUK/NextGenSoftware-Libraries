1e76f1add7a480401d05e315ac48c952c70ac32b
David Ellams <davidellams@hotmail.com>
2024-02-25 17:11:15 -0600

    - Added nextMessageOnSameLine param to Log methods in NLogProvider and also updated INLogProvider.

--- NextGenSoftware Libraries/NextGenSoftware.Logging.NLog/INLogProvider.cs
+++ NextGenSoftware Libraries/NextGenSoftware.Logging.NLog/INLogProvider.cs
@@ -3,8 +3,8 @@ namespace NextGenSoftware.Logging.NLogger
 {
     public interface INLogProvider
     {
-        void Log(string message, LogType type, bool showWorkingAnimation = false, bool logOnNewLine = true, bool insertExtraNewLineAfterLogMessage = false, int? indentLogMessagesBy = 1);
-        void Log(string message, LogType type, ConsoleColor consoleColour, bool showWorkingAnimation = false, bool logOnNewLine = true, bool insertExtraNewLineAfterLogMessage = false, int? indentLogMessagesBy = 1);
+        void Log(string message, LogType type, bool showWorkingAnimation = false, bool logOnNewLine = true, bool insertExtraNewLineAfterLogMessage = false, int? indentLogMessagesBy = 1, bool nextMessageOnSameLine = false);
+        void Log(string message, LogType type, ConsoleColor consoleColour, bool showWorkingAnimation = false, bool logOnNewLine = true, bool insertExtraNewLineAfterLogMessage = false, int? indentLogMessagesBy = 1, bool nextMessageOnSameLine = false);
         void Shutdown();
     }
 }
